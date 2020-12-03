d30637516b82a01c0be77539469a0976eba16fd9
Suthon <suthon.s@psu.ac.th>
2020-12-03 19:32:44 +0700

    [Add] aspect ratio

--- lib/presentation/film_detail.dart
+++ lib/presentation/film_detail.dart
@@ -29,13 +29,15 @@ class _FilmDetailPageState extends State<FilmDetailPage> {
         padding: const EdgeInsets.all(8),
         children: <Widget>[
           //https://starwars-visualguide.com/assets/img/films/4.jpg
-          Container(
-            height: 200,
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                  image: NetworkImage(
-                      "https://starwars-visualguide.com/assets/img/films/${film["id"]}.jpg"),
-                  fit: BoxFit.cover),
+          AspectRatio(
+            aspectRatio: 4 / 3,
+            child: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: NetworkImage(
+                        "https://starwars-visualguide.com/assets/img/films/${film["id"]}.jpg"),
+                    fit: BoxFit.cover),
+              ),
             ),
           ),
           Container(
